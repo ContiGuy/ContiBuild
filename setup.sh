5626caee7e8fb31f34d371645b8c32820ca6cb8f
ContiGuy <mrcs.contiguy@mailnull.com>
2016-07-13 17:19:36 +0200

    add copyright to wrapper scripts

--- setup.sh
+++ setup.sh
@@ -93,12 +93,27 @@ cat cb.sh |
 
 ## for tool in go gvt cobra ego elm psc pulp upx ; do
 for tool in go gvt cobra ego elm upx ; do
-	cat > "$TOOLS_DIR/$tool" <<EOF
+	## cat > "$TOOLS_DIR/$tool" <<EOF
+	cat <<EOF | sed -e "s%^# Copyright.*%$COPYRIGHT%" > "$TOOLS_DIR/$tool"
 #!/bin/bash
 #
-# wrapper for $tool
+# Copyright © 2016 - present:  Conti Guy  <mrcs.contiguy@mailnull.com>
 #
-$LICENSE
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# containerizing wrapper for $tool
+#
 
 cb $tool "\$@"
 
